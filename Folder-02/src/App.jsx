8f89b97e4b58040f507f02c6856f5625a04c27b6
adityabhosale11 <aadibhosale001@gmail.com>
2026-08-04 19:47:03 +0530

    Added Folder-2

---
+++ Folder-02/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from "./component/Card"
+import Navbar from './component/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Card />
+        <Card />
+        <Navbar/>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Folder-02/src/component/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Aditya Bhosale</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sed eligendi, omnis officia obcaecati sint doloremque repellat dolorum sapiente facere?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
