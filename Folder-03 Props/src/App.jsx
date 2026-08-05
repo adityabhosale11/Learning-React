e23c1efdb0875d8bfcc5d8f72356969316a095c9
adityabhosale11 <aadibhosale001@gmail.com>
2026-08-05 22:07:26 +0530

    Added Folder-3 Props

---
+++ Folder-03 Props/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='Parent'>
+      <div className="card">
+        <h1>Aditya Bhosale</h1>
+        <img src="https://images.unsplash.com/photo-1784613435069-aab1ab54e63a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, explicabo.</p>
+      <button>View Profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
